3642d948e7707e691c2f2be521310445b261486c
Sylvester Nkeze <snkeze9@gmail.com>
2021-06-28 22:43:59 +0100

    Added: Home page edited

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+(function ($) {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+})(jQuery)
