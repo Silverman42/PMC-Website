6572e3172ca705f489bd05cf9b19a6e6645fa891
Sylvester Nkeze <snkeze9@gmail.com>
2021-06-29 07:10:18 +0100

    Update: Index page updated

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,30 @@
         autoplayTimeout: 3000,
         autoplayHoverPause: true
     });
+
+
+    const serviceType = $('#serviceType');
+    const currentYear = new Date().getFullYear()
+    $('#copyDate').text(currentYear);
+    const dataBase= [
+        'trading',
+        'betting',
+        'telecomms'
+    ]
+    var state = 1
+    const changeContent = ()=>{
+        serviceType.text(dataBase[state - 1])
+        if(state === dataBase.length){
+            return state = 1
+        }
+        state += 1
+    }
+    const rotateContent = () => setTimeout(()=>{
+        changeContent()
+        console.log(state)
+        rotateContent()
+    },2000)
+
+    rotateContent()
+
 })(jQuery)
